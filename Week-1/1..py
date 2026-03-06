b3b60f6bbfd3e7bd73ef555ccaa13c656e36e915
Mukul Sahu <mukulsahu2005@gmail.com>
2026-03-06 13:37:46 +0530

    commit

---
+++ Week-1/1..py
@@ -0,0 +1,16 @@
+# name="Mukul Sahu"
+# age=20
+# height=147
+# print(name, type(name))
+# print(age, type(age))
+# print(height, type(height))
+
+# n1=int(input("Enter n1:"))
+# n2=int(input("Enter n2:"))
+# print("sum:", n1+n2)
+
+# age=int(input("Enter Age:"))
+# if age>=18 :
+#   print("Eligigble !")
+# else :
+#   print("Not eligible")
---
+++ Week-1/2.py
@@ -0,0 +1,18 @@
+# for i in range(1,11):
+#   if i%2==0 :
+#     print(i, "-EVEN")
+#   else :
+#     print(i,"-ODD")
+
+# arr=[2,3,1,11,-20]
+# arr.sort()
+# print(arr)
+# # arr.remove(0) #error - removing using value 
+# del arr[0]
+# print(arr)
+
+# arr=("mukul", "abc", "ram", "raju")
+# size=len(arr)
+# print(arr[0])
+# print(arr[size-1])
+
