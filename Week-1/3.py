e8a3ee1d790fec78194c2b1f98401e684526093b
Mukul Sahu <mukulsahu2005@gmail.com>
2026-03-06 14:42:22 +0530

    commit

---
+++ Week-1/3.py
@@ -0,0 +1,26 @@
+# Q1
+# def add_numbers (a,b):
+#   return a+b
+
+# result =add_numbers(10,23)
+# print(result )
+
+#Q2
+# def greet(name="guest"):
+#   print("hello",name) 
+
+# greet()
+# greet("mukul")
+
+#Q3
+# square=lambda x:x*x 
+# print (square(10))
+
+#q4
+# with open("sample.txt","w") as file: 
+#   for i in range(1,6):
+#     file.write(f"this is line {i} \n")
+
+# with open("sample.txt","r") as file:
+#   content=file.read()
+#   print(content)
